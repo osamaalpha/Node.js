91e1b3168906ea5835abf8b5ee1ac7d31520bbda
osamaalpha <osamamohamedupisdown@gmail.com>
2021-05-04 15:56:56 +0300

    fixing issues on the my blog file

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 app.use(express.json())
 // YOUR CODE GOES IN HERE
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -9,40 +10,48 @@ const fs = require("fs");
 app.post('/blogs', (req, res) => {
     // How to get the title and content from the request??
     fs.writeFileSync(req.body.title, req.body.content);
-    res.end('ok')
+    res.status(201).end('ok')
 })
 
 app.put('/posts/:title', (req, res) => {
   // How to get the title and content from the request?
   // What if the request does not have a title and/or content?
-  if (fs.existsSync(req.body.title)) {
-    fs.writeFileSync(req.body.title, req.body.content);
-    res.end('ok')
+  const {title,content}= req.body
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.status(200).end('ok')
   }
   else {
     // Send response with error message
-    res.end('not cool')
+    res.status(404).end('not cool')
   }
 })
 
 app.delete('/blogs/:title', (req, res) => {
+  const {title} = req.params
   // How to get the title from the url parameters?
-  if (fs.existsSync(req.params.title)) { // Add condition here
-    fs.unlinkSync(req.params.title);
-    res.end('ok');
+  if (fs.existsSync(title)) { // Add condition here
+    fs.unlinkSync(title);
+    res.status(404).end('ok');
   } else {
     // Respond with message here
-    res.end('not cool')
+    res.status(404).end('not cool')
   }
 })
 app.get('/blogs/:title', (req, res) => {
   // How to get the title from the url parameters?
-  const post = fs.readFileSync(req.params.title);
-  if (post) { 
-    res.end(post);
+  const {title} = req.params
+  if(!title){
+  res.status(400).end('please provide the title of the blog')
   }else{
-    res.end('ops')
+    const post = fs.readFileSync(title);
+  if (post) { 
+    res.status(200).end(post);
+  }else{
+    res.status(404).end('ops')
   }
+  }
+  
   // check if post exists
   
   // send response
